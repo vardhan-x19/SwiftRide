1e7636f9e9c963b56e05bb6c905d931a07e89b2e
Harsha <donpalharshavardhan07@gmail.com>
2025-02-07 16:56:23 +0530

    initial

---
+++ Backend/app.js
@@ -0,0 +1,17 @@
+const dotenv=require('dotenv');
+
+
+const express=require('express');
+const cors=require('cors');
+const app=express();  
+dotenv.config();
+app.use(cors());//only thise domian can aceess the api 
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+app.get('/',(req,res)=>{
+    res.send('Hello World');
+})
+
+
+module.exports= app;
